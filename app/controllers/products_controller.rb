2e163e4101f7d7ca52d08f6c06c67b0c7c29b04e
Seth <itz.saga@gmail.com>
2018-06-02 23:16:32 -0500

    Add tags to each endpoint

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    render json: @products
+    render json: @products, include: [:tags]
   end
 
   def show
     @product = Product.find(params[:id])
-    render json: @product
+    render json: @product, include: [:tags]
   end
 end
