280a6105a6ebe20c5faa3684b0dbc43e4b3c6c63
Seth <itz.saga@gmail.com>
2018-06-02 22:25:24 -0500

    Add associations in models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  has_many :user_products
+  has_many :users, through: :user_products
+  has_many :product_tags
+  has_many :tags, through: :product_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :user_products
+  has_many :products, through: :user_products
 end
