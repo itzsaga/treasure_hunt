928f0d857724f7dbd6752c9986838abbebbf2d49
Seth <itz.saga@gmail.com>
2018-06-03 18:51:49 -0500

    Add routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router } from '@reach/router'
+
 import App from './components/App'
 
 import './styles/base.sass'
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App path="/">
+    </App>
+  </Router>,
   document.getElementById('app')
 )
