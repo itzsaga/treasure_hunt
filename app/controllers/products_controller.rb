604476d9599da159d7e23159276af68278ed6d52
Seth <itz.saga@gmail.com>
2018-06-03 18:26:23 -0500

    Add product create endpoint

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,13 @@ class ProductsController < ApplicationController
     render json: @products, include: [:tags]
   end
 
+  def create
+    @product = Product.new
+    if @product.save
+      render json: @product
+    end
+  end
+
   def show
     @product = Product.find(params[:id])
     render json: @product, include: [:tags]
