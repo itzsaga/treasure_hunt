4030196e7f17c1bbbf398e35a2d0362876065b6d
Seth <itz.saga@gmail.com>
2018-06-03 19:05:32 -0500

    Implement Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@reach/router'
+import { Provider } from 'react-redux'
 
 import App from './components/App'
+import store from './redux/store'
 
 import './styles/base.sass'
 
 ReactDOM.render(
-  <Router>
-    <App path="/">
-    </App>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App path="/">
+      </App>
+    </Router>
+  </Provider>,
   document.getElementById('app')
 )
---
+++ client/src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+const rootReducer = combineReducers({
+})
+
+export default createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+)
