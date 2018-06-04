7355318b5d03193df4ce19f24b15a7efe2f7f4fd
Seth <itz.saga@gmail.com>
2018-06-04 00:10:34 -0500

    Add links

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from '@reach/router'
 import icon from '../images/th-icon.jpg'
 
 const ProductList = ({ products, searching }) => {
   const title = searching ? <h2 className="title is-2">Search Results:</h2> : <h2 className="title is-2">Latest Treasures:</h2>
-  const productsList = products.map((product, i) => (
+  const productsList = products.map((product, i) => {
+    const linkTo = `product/${product.id}`
+    return (
     <div className="box" key={product.id}>
       <div className="media">
         <div className="media-left">
@@ -15,12 +18,13 @@ const ProductList = ({ products, searching }) => {
           <p><strong>{product.name}</strong></p>
           <p>{product.description}</p>
           <div id="more-info-button">
-            <button value={product.id} className="button is-info">More Info</button>
+            <Link to={linkTo}><button value={product.id} className="button is-info">More Info</button></Link>
           </div>
         </div>
       </div>
     </div>
-  ))
+    )
+  })
   return (
     <div id="products">
       {title}
