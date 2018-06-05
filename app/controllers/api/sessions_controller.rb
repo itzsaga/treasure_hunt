8f52af8d02125d9a971bc2645ed6083acea4f050
Seth <itz.saga@gmail.com>
2018-06-04 23:02:40 -0500

    Add token to login

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,7 +2,8 @@ class Api::SessionsController < ApplicationController
   def login
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
-      render 
+      token = Auth.encode(@user.id)
+      render json: { token: token, user: @user }
     else
       # send an error
     end
