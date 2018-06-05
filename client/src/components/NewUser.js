b63fd51be33cd53095dbff1899c808be15d8ed32
Seth <itz.saga@gmail.com>
2018-06-04 22:59:52 -0500

    Parents of UserForm component

---
+++ client/src/components/NewUser.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import UserForm from './UserForm'
+
+class NewUser extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Create a new User</h1>
+        <UserForm type="signup" history={this.props.history} />
+      </div>
+    )
+  }
+}
+
+export default NewUser
