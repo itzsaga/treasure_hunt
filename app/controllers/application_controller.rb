c3f50a68102841f72eb37fe260953a616d49ea1a
Seth <itz.saga@gmail.com>
2018-06-04 23:58:22 -0500

    Move auth to sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def encode(user_id)
+    payload = {user_id: user_id}
+    JWT.encode payload, 'my_secret', 'HS256'
+  end
+
+  def decode(token)
+    JWT.decode token, 'my_secret', true, { algorithm: 'HS256' }
+  end
 end
