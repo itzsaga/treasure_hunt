ce05dc733a78fc755bc7c0f465b45df3021b4a33
Seth <itz.saga@gmail.com>
2018-06-04 22:51:52 -0500

    Add login action

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -16,3 +16,20 @@ export const createUser = user => {
       })
   }
 }
+
+export const login = user => {
+  return dispatch => {
+    return fetch(`http://localhost:3000/api/login`, {
+      method: 'POST',
+      body: JSON.stringify({ user }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(({ user, token }) => {
+        dispatch({ type: 'AUTH_COMPLETE', user })
+        localStorage.setItem('token', token)
+      })
+  }
+}
