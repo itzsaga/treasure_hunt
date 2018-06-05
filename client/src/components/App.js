105a438a752f8fb13e0cb0cf6efafa91af25617d
Seth <itz.saga@gmail.com>
2018-06-04 23:02:22 -0500

    Route new components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,12 +4,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from './Home'
 import Product from './Product'
+import Login from "./Login";
+import NewUser from "./NewUser";
 
 const App = () =>
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route path="product/:productId" component={Product} />
+      <Route path="/product/:productId" component={Product} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={NewUser} />
     </Switch>
   </Router>
 
