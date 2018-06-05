dbdc9ca68f57b67002726b48368057807f3f8be4
Seth <itz.saga@gmail.com>
2018-06-04 22:06:56 -0500

    Add create method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,22 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      token = Auth.encode(@user.id)
+      render json: { token: token, user: @user }
+    else
+      # send error
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     render json: @user, include: [:products]
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
   end
 end
