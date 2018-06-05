d67163f70a48c1e99a5e970154525b0385e92f21
Seth <itz.saga@gmail.com>
2018-06-04 22:45:56 -0500

    Swap in react-router-dom

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from '@reach/router'
+import { Link } from 'react-router-dom'
 import Fuse from 'fuse.js'
 import { fuseOptions } from '../fuseOptions'
 import icon from '../images/th-icon.jpg'
