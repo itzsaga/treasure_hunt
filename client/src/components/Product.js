93f9303c897e6d6df0c264083e97e662cd847be9
Seth <itz.saga@gmail.com>
2018-06-06 23:02:56 -0500

    Product show component

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import icon from '../images/th-icon.jpg'
+
 class Product extends Component {
   state = {
-    product: {}
+    product: this.props.products.find(p => p.id == this.props.match.params.productId),
   }
 
-  componentDidMount() {
-    const products = this.props.products
-    const product = products.find(p => p.id == this.props.match.params.productId)
-    this.setState({
-      product: product
-    })
+  renderTags() {
+    return this.state.product.tags.map(tag => (
+      <span className="tag is-light is-rounded is-medium">{tag.name}</span>
+    ))
   }
 
   render() {
     return(
-      <div>
-        {`${this.state.product}`}
+      <div className="container">
+        <div className="box" key={this.state.product.id}>
+          <div className="media">
+            <div className="media-left">
+              <figure className="image">
+                <img src={`../../${icon}`} alt="product image" />
+              </figure>
+            </div>
+            <div className="media-content">
+              <p><strong>{this.state.product.name}</strong></p>
+              <p>{this.state.product.description}</p>
+              <div className="tags">
+                {this.renderTags()}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
